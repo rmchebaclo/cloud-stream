936be13eeee5b08d103ca8ccd2fb5f6b6b302740
Roy Chebaclo <rmchebaclo@gmail.com>
2024-06-03 20:19:32 -0500

    Process videos locally using Thunder Client

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,40 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg'
+
 
 const app = express();
-const port = 3000;
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/process-video', (req, res) => {
+  // Get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  // error throwing for missing files
+  if (!inputFilePath && !outputFilePath) {
+    res.status(400).send("Bad Request: Missing input and output file path")
+  }
+  else if (!inputFilePath) {
+    res.status(400).send("Bad Request: Missing input file path")
+  } 
+  else if (!outputFilePath) {
+    res.status(400).send("Bad Request: Missing output file path")
+  }
+
+  ffmpeg(inputFilePath).outputOptions("-vf", "scale= 1920:1080") // 1080p
+
+  .on("end", () => {
+    return res.status(200).send("Video processing finished succesfully.")
+  })
+  .on("error", (err) => {
+    console.log(`An error occured: ${err.message}`)
+    res.status(500).send(`Internal Server Error: ${err.message}`)
+  })
+  .save(outputFilePath)
+  
 });
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
